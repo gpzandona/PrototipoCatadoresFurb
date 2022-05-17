12a513d18413c2c1efe65f3977980172d0b47a2f
Gabriel Pietro Zandona <gpandona@gmail.com>
2022-05-17 13:36:40 -0300

    começando CRUD e testando pacote brasileiro

---
+++ lib/data/dummy_users.dart
@@ -0,0 +1,17 @@
+import 'package:teste_flutter/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': User(id: '1',
+      name: 'Luis Vogel',
+      email: 'avogel@gmail.com',
+      phoneNumber: "47992857563",
+      cpf: '000000000000',
+      birthDate: '17/07/1970'),
+
+  '2': User(id: '2',
+      name: 'Thais Vogel',
+      email: 'tvogel@gmail.com',
+      phoneNumber: "4799792812",
+      cpf: '000000000001',
+      birthDate: '18/08/1978'),
+};
---
+++ lib/models/user.dart
@@ -0,0 +1,25 @@
+class User {
+  final String id;
+  final String name;
+  final String email;
+  final String phoneNumber;
+  final String cpf;
+  final String birthDate;
+
+  /*final bool zap;
+  final bool zapMsg;
+  final String email;
+  final String gender;
+  final String birthDate;
+  final String motherName;*/
+
+  const User({
+    required this.id, 
+    required this.name,
+    required this.email,
+    required this.phoneNumber,
+    required this.cpf,
+    required this.birthDate,
+  });
+
+}
